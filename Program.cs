f7bf97064f6c2024cac70c2b14d0a5d15c5816ae
Kieran Emery <kizwiz@hotmail.co.uk>
2023-02-03 19:07:31 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] pickupLines =
+        {
+            "Are you a magician? Because whenever I look at you, everyone else disappears.",
+            "Do you have a map? Because I just got lost in your eyes.",
+            "I must be a snowflake, because I've fallen for you.",
+            "Do you have a sunburn, or are you always this hot?",
+            "If you were a vegetable, you'd be a cutecumber."
+        };
+
+        introductionText();
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.ReadLine();
+
+        Random random = new Random();
+        int lineIndex = random.Next(pickupLines.Length);
+
+        Console.WriteLine(pickupLines[lineIndex]);
+        Console.ReadLine();
+    }
+
+    static void introductionText()
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Welcome to the Pick-Up Line Generator!");
+        Console.WriteLine("Press Enter to generate a line...");
+    }
+}
